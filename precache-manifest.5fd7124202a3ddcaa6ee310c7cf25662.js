4ea26d70183688aad52196a27db2a8bc489ff975
RoboCopGay <vinicios.sousa909@gmail.com>
2019-10-18 00:07:05 -0300

    Updates

---
+++ precache-manifest.5fd7124202a3ddcaa6ee310c7cf25662.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "610cceecc788742a8b225b76bcaecf95",
+    "url": "/AgroTechProject/index.html"
+  },
+  {
+    "revision": "9bd5c9177df170697d55",
+    "url": "/AgroTechProject/static/css/main.147fa733.chunk.css"
+  },
+  {
+    "revision": "918a454dab3d97aa075a",
+    "url": "/AgroTechProject/static/js/2.573d791c.chunk.js"
+  },
+  {
+    "revision": "9bd5c9177df170697d55",
+    "url": "/AgroTechProject/static/js/main.5292bfd3.chunk.js"
+  },
+  {
+    "revision": "4d1da2f7b642d2bf8274",
+    "url": "/AgroTechProject/static/js/runtime-main.3da8db2a.js"
+  },
+  {
+    "revision": "32bf719089f7caf77c0ea2e90d850771",
+    "url": "/AgroTechProject/static/media/camera.32bf7190.svg"
+  },
+  {
+    "revision": "05b0b26cc1b56b86b435a764433e1579",
+    "url": "/AgroTechProject/static/media/logo.05b0b26c.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-gh-pages/precache-manifest.939de27ad6dc0f32fb7497ccd21437ef.js"
+  "/AgroTechProject/precache-manifest.5fd7124202a3ddcaa6ee310c7cf25662.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-gh-pages/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/AgroTechProject/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
